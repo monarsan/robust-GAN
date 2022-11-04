6b1a57287e9348f9e953115242fce5ff7bf279b3
monarsan <aaaaa333445@gmail.com>
2022-11-04 18:08:58 +0900

    add code for multivar

---
+++ work/multivar/exp.sh
@@ -0,0 +1,21 @@
+n="1000"
+eps="0.1"
+data_dim="10"
+mu="5"
+mu_out="0"
+par_reg1="0.0005"
+lr="0.05"
+decay_par="0.5"
+exper_iter="10"
+optim_iter="500"
+mm_iter="1"
+optim_method="BFGS"  #Nelder-Mead, SLSQP BFGS
+init_loc="0"
+
+
+#Do python
+for n in 100 1000 2000 3000
+do
+    python3 ./multivar_complex_u.py "$n" "$eps" "$data_dim" "$mu" "$mu_out" "$exper_iter" "$optim_iter" "$mm_iter" "$optim_method" "$decay_par" "$par_reg1" "$lr" "$init_loc"
+done
+
