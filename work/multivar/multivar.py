bc0724084423e8d2303a83fb303249f3d06ebb61
monarsan <aaaaa333445@gmail.com>
2022-11-11 11:04:45 +0900

    change:init bias is mean of u(x)

--- work/multivar/multivar.py
+++ work/multivar/multivar.py
@@ -61,7 +61,7 @@ for i in range(exper_iter):
     z = np.random.multivariate_normal(mean=alpha[0], cov=alpha[1], size = m)
     #par = np.random.normal(loc = 0, scale = 0.1, size = 2*data_dim + 1)
     par = np.random.normal(loc = 0, scale = 0.1, size = 2*data_dim)
-    bias = np.array(np.median(np.dot(np.stack([z, z**2], axis=1).reshape(m, 2*data_dim),par[0:2*data_dim])))[np.newaxis]
+    bias = np.array(np.mean(np.dot(np.stack([z, z**2], axis=1).reshape(m, 2*data_dim),par[0:2*data_dim])))[np.newaxis]
     par = np.concatenate([par, bias], axis = 0)
     for j in tqdm(range(1, optim_iter+1)):
         z = np.random.multivariate_normal(mean=alpha[0], cov=alpha[1], size = m)
