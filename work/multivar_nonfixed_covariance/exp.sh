6108ecd2a6ffaa86b15505c219998c2c807602df
monarsan <aaaaa333445@gmail.com>
2022-11-11 15:01:24 +0900

    add:folder of multivar nonfixed cov

---
+++ work/multivar_nonfixed_covariance/exp.sh
@@ -0,0 +1,19 @@
+n="1000"
+eps="0.1"
+data_dim="2"
+mu="0"
+mu_out="5"
+par_reg1="0.0005"
+lr="0.05"
+decay_par="0.5"
+exper_iter="10"
+optim_iter="500"
+mm_iter="1"
+optim_method="BFGS"  #Nelder-Mead, SLSQP BFGS
+init_loc="0"
+
+
+#Do python
+
+python3 ./multivar_nonfixed_cov.py "$n" "$eps" "$data_dim" "$mu" "$mu_out" "$exper_iter" "$optim_iter" "$mm_iter" "$optim_method" "$decay_par" "$par_reg1" "$lr" "$init_loc"
+
