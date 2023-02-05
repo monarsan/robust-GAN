0858bf7469b2682cb10b7db90706f0c7a280895d
monarsan <aaaaa333445@gmail.com>
2023-02-06 03:37:51 +0900

    2/6

--- work/main/for_grad.sh
+++ work/main/for_grad.sh
@@ -1,21 +1,15 @@
-for eps in 0.05 0.1 0.15 0.2
+for eps in 0.02 0.05 0.1 0.15 0.2
 do
-    python3 grad_paper.py 5 10000 $eps change_eps
+    python3 grad_paper.py 2 4000 $eps change_eps_dim2
+done
+
+for dim in 2 4 6 8 10 15 20 25
+do
+    python3 grad_paper.py $dim 1000 0.1 change_dim
 done
 
 
-for n in 100 200 400 700 1000
+for n in 50 100 200 400 700 1000
 do
     python3 grad_paper.py 10 $n 0.1 change_n
-done
-
-
-for dim in 25 50
-do
-    python3 grad_paper.py $dim 1000 0.1 change_dim    
-done 
-
-for eps in 0.05 0.1 0.15 0.2
-do
-    python3 grad_paper.py 10 10000 $eps change_eps_dim10
 done
--- work/main/grad_paper.py
+++ work/main/grad_paper.py
@@ -14,35 +14,42 @@ data_dim = int(args.dim)
 data_size = int(args.n)
 eps = float(args.eps)
 eps_int = int(100 * eps)
+dir = 'images_for_grad_mean0_out5'
 
 exp_name = f'{args.exp_name}/dim{data_dim}n{data_size}eps{eps_int}'
-os.makedirs(f'images_for_grad2/{exp_name}', exist_ok=True)
+os.makedirs(f'{dir}/{exp_name}', exist_ok=True)
 
-
+print(f'start {exp_name}')
 loss = []
 for i in range(10):
+    print(f'{i}-th iteration')
     gan = GAN(data_dim=data_dim, eps=eps)
-    gan.dist_init(setting='mu', true_mean=5, out_mean=0)
+    gan.dist_init(setting='mu', true_mean=0, out_mean=5)
     gan.data_init(data_size=data_size, mc_ratio=3)
     gan.model_init()
-    gan.optimizer_init(lr_d=0.2, lr_g=0.02, decay_par=0.4, reg_d=6e-5, reg_g=6e-5, update_D_iter=5)
+    gan.optimizer_init(lr_d=1, lr_g=0.02, decay_par=0.4,
+                       reg_d=6e-5, reg_g=5e-5, update_D_iter=1)
     gan.fit(optim_iter=3000)
     loss.append(gan.score(100))
     # plt.subplots()
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
     plt.plot(gan.l2_loss)
     plt.xlabel('optim step')
     plt.ylabel('l2 loss')
-    plt.title(f'data dim is {gan.data_dim}')
+    plt.title(f'd={gan.data_dim}, n={data_size}, eps={eps}')
     plt.subplot(1, 2, 2)
     plt.plot(np.array(gan.G_record))
     plt.xlabel('optim step')
     plt.ylabel('estimated value of mean')
-    plt.title(f'data dim is {gan.data_dim}')
-    plt.savefig(f'images_for_grad/{exp_name}-{i}.png')
+    plt.title(f'd={gan.data_dim}, n={data_size}, eps={eps}')
+    plt.savefig(f'{dir}/{exp_name}/{i}.png')
 loss = np.array(loss)
 
-with open(f'images_for_grad2/{exp_name}.txt', 'w') as f:
-    f.write(f'loss mean is {np.mean(loss)}')
-    f.write(f'loss std  is {np.std(loss)}')
+with open(f'{dir}/loss_mean.txt', 'w') as f:
+    f.write(f'{np.mean(loss)},\n')
+    
+with open(f'{dir}/loss_std.txt', 'w') as f:   
+    f.write(f'{np.std(loss)},\n')
+    
+print(f'end {exp_name}')
