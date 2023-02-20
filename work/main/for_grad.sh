a44f0dd6fa45e87fea214436e005f287f13af8ea
monarsan <aaaaa333445@gmail.com>
2023-02-21 00:11:43 +0900

    do exp changing decay and clip

--- work/main/for_grad.sh
+++ work/main/for_grad.sh
@@ -1,15 +1,9 @@
-for eps in 0.02 0.05 0.1 0.15 0.2
+for decay in 0.3 0.4 0.5 0.6 0.7
 do
-    python3 grad_paper.py 2 4000 $eps change_eps_dim2
+    python3 grad_paper.py 100 1000 0.1 dim100_decay $decay 1 0.1
 done
 
-for dim in 2 4 6 8 10 15 20 25
+for clip in 0.1 0.01 0.001 0.0001
 do
-    python3 grad_paper.py $dim 1000 0.1 change_dim
-done
-
-
-for n in 50 100 200 400 700 1000
-do
-    python3 grad_paper.py 10 $n 0.1 change_n
+    python3 grad_paper.py 100 1000 0.1 dim100 0.4 1 $clip
 done
--- work/main/grad_paper.py
+++ work/main/grad_paper.py
@@ -9,14 +9,21 @@ parser.add_argument('dim')
 parser.add_argument('n')
 parser.add_argument('eps')
 parser.add_argument('exp_name')
+parser.add_argument('decay_par')
+parser.add_argument('l_smooth')
+parser.add_argument('grad_clip')
+
 args = parser.parse_args()
 data_dim = int(args.dim)
 data_size = int(args.n)
 eps = float(args.eps)
 eps_int = int(100 * eps)
-dir = 'images_for_grad_mean5_out0_strong_reg_g'
+decay_par = float(args.decay_par)
+l_smooth = float(args.l_smooth)
+grad_clip = float(args.grad_clip)
+dir = 'images_for_grad_dim100_change_decay_clip'
 
-exp_name = f'{args.exp_name}/dim{data_dim}n{data_size}eps{eps_int}'
+exp_name = f'{args.exp_name}/decay{decay_par * 10}clip{grad_clip * 1e4}'
 os.makedirs(f'{dir}/{exp_name}', exist_ok=True)
 
 print(f'start {exp_name}')
@@ -27,22 +34,28 @@ for i in range(5):
     gan.dist_init(setting='mu', true_mean=5, out_mean=0)
     gan.data_init(data_size=data_size, mc_ratio=3)
     gan.model_init()
-    gan.optimizer_init(lr_d=1, lr_g=0.4, decay_par=0.4,
-                       reg_d=6e-5, reg_g=5e-4, update_D_iter=1)
+    gan.optimizer_init(lr_d=1, lr_g=1, decay_par=decay_par,
+                       reg_d=6e-5, reg_g=5e-4, update_D_iter=1, 
+                       l_smooth=l_smooth, grad_clip=grad_clip)
     gan.fit(optim_iter=1000, verbose=True)
     loss.append(gan.score(100))
     # plt.subplots()
     plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.plot(gan.l2_loss)
     plt.xlabel('optim step')
     plt.ylabel('l2 loss')
     plt.title(f'd={gan.data_dim}, n={data_size}, eps={eps}')
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.plot(np.array(gan.G_record))
     plt.xlabel('optim step')
     plt.ylabel('estimated value of mean')
     plt.title(f'd={gan.data_dim}, n={data_size}, eps={eps}')
+    plt.subplot(1, 3, 3)
+    plt.plot(gan.D_data_record, label='target')
+    plt.plot(gan.D_contami_record,label='contami')
+    plt.plot(gan.D_z_record, label='z')
+    plt.legend()
     plt.savefig(f'{dir}/{exp_name}/{i}.png')
 loss = np.array(loss)
 
