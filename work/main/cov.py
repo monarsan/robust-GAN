5d376ccd905e2871998b555987c7a233a1a8196f
monarsan <aaaaa333445@gmail.com>
2023-04-10 15:36:53 +0900

    updata:dir structure

--- work/main/cov.py
+++ work/main/cov.py
@@ -3,38 +3,17 @@ import matplotlib.pyplot as plt
 from gan import gan as GAN
 
 
-optim_iter = 950
+optim_iter = 550
+n = 50000
 for i in range(10):
-    gan = GAN(5, 0.2)
-    gan.dist_init('sigma', 0, 6, sigma_setting='ar')
-    gan.data_init(50000, 1)
-    gan.model_init(D_init_option='random', G_init_option='kendall')
-    gan.optimizer_init(lr_d=1, lr_g=0.1, decay_g=0.2, reg_g=1e-4, reg_d=1e-4,
-                       update_D_iter=5, is_mm_alg=False, decay_d=1, grad_clip=1e-3, lr_schedule='step', step=200)
-    gan.fit(optim_iter, verbose=True)
-    gan.record_npy(rcd_dir='record', rcd_name=f'est_sig_dim{gan.data_dim}_{i}')
-    gan.plot(fig_scale=1)
-    plt.show()
-    
-    gan = GAN(10, 0.2)
-    gan.dist_init('sigma', 0, 6, sigma_setting='ar')
-    gan.data_init(50000, 1)
-    gan.model_init(D_init_option='random', G_init_option='kendall')
-    gan.optimizer_init(lr_d=1, lr_g=0.1, decay_g=0.2, reg_g=1e-4, reg_d=1e-4,
-                       update_D_iter=5, is_mm_alg=False, decay_d=1, grad_clip=1e-3, lr_schedule='step', step=200)
-    gan.fit(optim_iter, verbose=True)
-    gan.record_npy(rcd_dir='record', rcd_name=f'est_sig_dim{gan.data_dim}_{i}')
-    gan.plot(fig_scale=1)
-    plt.show()
-    
-    gan = GAN(25, 0.2)
-    gan.dist_init('sigma', 0, 6, sigma_setting='ar')
-    gan.data_init(50000, 1)
-    gan.model_init(D_init_option='random', G_init_option='kendall')
-    gan.optimizer_init(lr_d=1, lr_g=0.1, decay_g=0.2, reg_g=1e-4, reg_d=1e-4,
-                       update_D_iter=5, is_mm_alg=False, decay_d=1, grad_clip=1e-3, lr_schedule='step', step=200)
-    gan.fit(optim_iter, verbose=True)
-    gan.record_npy(rcd_dir='record', rcd_name=f'est_sig_dim{gan.data_dim}_{i}')
-    gan.plot(fig_scale=1)
-    plt.show()
+    for j in [5, 10, 25]:
+        gan = GAN(j, 0.2)
+        gan.dist_init('sigma', 0, 6, sigma_setting='ar')
+        gan.data_init(n, 5)
+        gan.model_init(D_init_option='random', G_init_option='kendall')
+        gan.optimizer_init(lr_d=1, lr_g=0.2, decay_g=0.2, reg_g=1e-4, reg_d=1e-4,
+                           update_D_iter=5, is_mm_alg=False, decay_d=1, grad_clip=1e-3, lr_schedule='step', step=400)
+        gan.fit(optim_iter, verbose=True)
+        gan.record_npy(rcd_dir='record50k-highg', rcd_name=f'est_sig_dim{gan.data_dim}_{i}')
+        gan.plot(fig_scale=1)
     
