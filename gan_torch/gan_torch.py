89579351c0de8c8677082d6bbb8f8ae05a66ad29
monarsan <aaaaa333445@gmail.com>
2023-05-01 22:40:50 +0900

    fix:bug

--- gan_torch/gan_torch.py
+++ gan_torch/gan_torch.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from data import contaminated_data
 from model import Discriminator_mu, Generator_mu
 from torch.utils.data import DataLoader
+from tqdm import trange
 
 
 class gan():
@@ -11,7 +12,7 @@ class gan():
         super().__init__()
         self.data_dim = data_dim
         self.eps = eps
-         self.device = device if device is not None \
+        self.device = device if device is not None \
                       else torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     
     def dist_init(self, true_mean, out_mean, true_cov, out_cov):
@@ -34,24 +35,24 @@ class gan():
         self.batch_size = batch_size
         self.dataloader = DataLoader(self.data, batch_size=batch_size, shuffle=True)
 
-class mu(gan):
+
+class Mu(gan):
     def __init__(self, data_dim, eps, device) -> None:
         super().__init__(data_dim, eps, device)
         
     def dist_init(self, true_mean, out_mean, true_cov=None, out_cov=None):
         super().dist_init(true_mean, out_mean, true_cov, out_cov)
-        self.true_cov = torch.tensor(true_cov, dtype=torch.float32)
-        self.out_cov = torch.tensor(out_cov, dtype=torch.float32)
+        self.true_cov = torch.eye(self.data_dim, dtype=torch.float32)
+        self.out_cov = torch.eye(self.data_dim, dtype=torch.float32)
     
     def data_init(self, data_size, batch_size):
-        super().dist_init(data_size, batch_size)
-        
+        super().data_init(data_size, batch_size)
         
     def model_init(self):
         self.D = Discriminator_mu(self.data_dim).to(self.device)
         self.G = Generator_mu(self.data_dim).to(self.device)
-        data_median = torch.median(self.data.data, dim=0)
-        self.G.data = data_median
+        data_median = torch.median(self.data.data, dim=0)[0]
+        self.G.est_mean.data = data_median
         
     def optimizer_init(self, lr_d, lr_g, d_steps, g_steps):
         self.D_optimizer = torch.optim.SGD(self.D.parameters(), lr=lr_d)
@@ -60,37 +61,74 @@ class mu(gan):
         self.g_steps = g_steps
     
     def fit(self, epochs):
-        self.loss_D = [] 
+        self.loss_D = []
         self.loss_G = []
         self.mean_err_record = []
         self.mean_est_record = []
-        z_b = torch.zeros(self.batch_size, self.p).to(self.device)
+        z_b = torch.zeros(self.batch_size, self.data_dim).to(self.device)
         one_b = torch.ones(self.batch_size).to(self.device)
-        criterion = torch.nn.BCELoss()
+        criterion = torch.nn.BCEWithLogitsLoss()
         current_d_step = 1
-        for ep in range(epochs):
+        for ep in trange(epochs):
             loss_D_ep = []
             loss_G_ep = []
             for _, data in enumerate(self.dataloader):
                 # train D
                 self.D.train()
                 self.D.zero_grad()
-                # D loss
+                #  D loss
                 x_real = data.to(self.device)
                 d_real_score = self.D(x_real)
-                d_real_loss = d_real_score.mean()
-                d_real_loss = criterion(d_real_loss, z_b)
-                # G loss
-                x_fake = self.G(z_b)
+                d_real_loss = d_real_score
+                d_real_loss = criterion(d_real_loss, 1 - one_b)
+                #  G loss
+                x_fake = self.G(z_b.normal_())
                 d_fake_score = self.D(x_fake)
-                d_fake_score = d_fake_score.mean()
+                d_fake_score = d_fake_score
                 d_fake_loss = criterion(d_fake_score, one_b)
-                d_loss = d_fake_loss - d_real_loss
+                d_loss = d_fake_loss + d_real_loss
                 d_loss.backward()
                 loss_D_ep.append(d_loss.item())
+                self.D_optimizer.step()
+                if current_d_step < self.d_steps:
+                    current_d_step += 1
+                    continue
+                else:
+                    current_d_step = 1
+                
+                #  train G
+                self.D.eval()
+                for _ in range(self.g_steps):
+                    self.G.zero_grad()
+                    x_fake = self.G(z_b.normal_())
+                    d_fake_score = self.D(x_fake)
+                    d_fake_score = d_fake_score
+                    g_loss = criterion(d_fake_score, 1 - one_b)
+                    g_loss.backward()
+                    loss_G_ep.append(g_loss.item())
+                    self.G_optimizer.step()
+            self.mean_err_record.append(
+                (self.G.est_mean.data - self.true_mean).norm(p=2).item()
+            )
+            self.mean_est_record.append(self.G.est_mean.data)
+            self.loss_D.append(np.mean(loss_D_ep))
+            self.loss_G.append(np.mean(loss_G_ep))
+    
+    def plot(self):
+        plt.plot(self.loss_D)
+        plt.title('loss_D')
+        plt.show()
+        
+        plt.plot(self.loss_G)
+        plt.title('loss_G')
+        plt.show()
+        
+        plt.plot(self.mean_err_record)
+        plt.title('Error')
+        plt.show()
 
                 
-class sigma(gan):
+class Sigma(gan):
     def __init__(self, data_dim, eps, device) -> None:
         super().__init__(data_dim, eps, device)
         
--- gan_torch/model.py
+++ gan_torch/model.py
@@ -4,19 +4,21 @@ import torch.nn as nn
 
 class Discriminator_mu(nn.Module):
     def __init__(self, data_dim) -> None:
+        super().__init__()
         self.data_dim = data_dim
-        self.parameters = nn.Parameter(torch.zoros(self.data_dim))
+        self.params = nn.Parameter(torch.randn(self.data_dim) * 0.1)
         self.bias = nn.Parameter(torch.zeros(1))
         
     def forward(self, x):
-        return nn.Sigmoid(torch.sum(x * self.parameters, dim=1) - self.bias)
+        return torch.sum(x * self.params, dim=1) - self.bias
     
     
 class Generator_mu(nn.Module):
     def __init__(self, data_dim) -> None:
+        super().__init__()
         self.data_dim = data_dim
-        self.mu = nn.parameter(torch.zeros(self.data_dim))
+        self.est_mean = nn.Parameter(torch.zeros(self.data_dim))
         
     def forward(self, x):
-        return self.mu + x
+        return self.est_mean + x
     
