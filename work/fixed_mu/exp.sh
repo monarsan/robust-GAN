85808f7708528a6dc59319e6444081dd640e35b7
Kodai Yokoyama <aaaaa333445@gmail.com>
2023-05-05 12:08:23 +0900

    aa

--- work/fixed_mu/exp.sh
+++ work/fixed_mu/exp.sh
@@ -1,13 +1,13 @@
 n="1000"
 eps="0.1"
-data_dim="2"
+data_dim="15"
 mu="0"
 mu_out="6"
 par_reg1="0.0005"
 lr="0.005"
 decay_par="0.3"
-exper_iter="10"
-optim_iter="1000"
+exper_iter="1"
+optim_iter="100"
 mm_iter="1"
 optim_method="BFGS"  #Nelder-Mead, SLSQP BFGS
 init_loc="0"
@@ -16,5 +16,5 @@ init_loc="0"
 #Do python
 for decay_par in 0.1
 do
-    python3 ./fixed_mu.py "$n" "$eps" "$data_dim" "$mu" "$mu_out" "$exper_iter" "$optim_iter" "$mm_iter" "$optim_method" "$decay_par" "$par_reg1" "$lr" "$init_loc"
+    python3 ./mat_cuda.py "$n" "$eps" "$data_dim" "$mu" "$mu_out" "$exper_iter" "$optim_iter" "$mm_iter" "$optim_method" "$decay_par" "$par_reg1" "$lr" "$init_loc"
 done
